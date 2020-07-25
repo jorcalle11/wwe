a9ccf7b6ef5bc3ec9522cdf9985a0b34a497a598
jorcalle11 <jorcalle11@gmail.com>
2020-07-25 14:41:34 -0500

    Lifting selectedNavItem state at App component

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -2,56 +2,31 @@ import React from 'react';
 import styled from 'styled-components';
 
 import Sidebar from './components/SideBar';
-import MainContent from './components/MainContent/MainContent';
+import Header from './components/Header';
+import CardList from './components/CardList';
 
-const GridContainer = styled.div`
+function App() {
+  const [selectedNavItem, setSelectedNavItem] = React.useState('todos');
+
+  return (
+    <Grid>
+      <Sidebar onSelectNavItem={setSelectedNavItem} />
+      <Main>
+        <Header selectedNavItem={selectedNavItem} />
+        <CardList selectedNavItem={selectedNavItem} />
+      </Main>
+    </Grid>
+  );
+}
+
+const Grid = styled.div`
   display: grid;
   grid-template-columns: 200px auto;
   height: 100%;
 `;
 
-const GridItem = styled.div`
-  // border: 1px solid aqua;
+const Main = styled.main`
+  height: 100%;
 `;
-
-function App() {
-  const [currentTab, setCurrentTab] = React.useState('todos');
-  const [entities, dispatch] = React.useReducer(reducer, {
-    todos: [],
-    users: [],
-    posts: []
-  });
-
-  return (
-    <GridContainer>
-      <GridItem>
-        <Sidebar
-          currentTab={currentTab}
-          setCurrentTab={setCurrentTab}
-          dispatch={dispatch}
-        />
-      </GridItem>
-      <GridItem>
-        <MainContent currentEntity={currentTab} entities={entities} />
-      </GridItem>
-    </GridContainer>
-  );
-}
-
-function reducer(state, action) {
-  switch (action.type) {
-    case 'TODOS': {
-      return { ...state, todos: action.data };
-    }
-    case 'USERS': {
-      return { ...state, users: action.data };
-    }
-    case 'POSTS': {
-      return { ...state, posts: action.data };
-    }
-    default:
-      return state;
-  }
-}
 
 export default App;
