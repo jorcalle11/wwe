7dcee1f9c1ccd527e44f097e5285a201f998e151
jorcalle11 <jorcalle11@gmail.com>
2020-07-25 14:50:15 -0500

    Add Todo Component and fetch data

--- web-app/src/components/CardList/CardList.js
+++ web-app/src/components/CardList/CardList.js
@@ -2,6 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
+import Api from '../../apis';
+
 export default function CardList({ selectedNavItem }) {
   if (selectedNavItem === 'users') {
     return 'users';
@@ -11,9 +13,61 @@ export default function CardList({ selectedNavItem }) {
     return 'posts';
   }
 
-  return 'todos';
+  return <Todos />;
 }
 
 CardList.propTypes = {
   selectedNavItem: PropTypes.string.isRequired
 };
+
+function Todos() {
+  const [todos, setTodos] = React.useState([]);
+  const [status, setStatus] = React.useState('started');
+  const [error, setError] = React.useState(null);
+
+  React.useEffect(() => {
+    const abortController = new AbortController();
+
+    Api.fetchDataByEntity('todos', { signal: abortController.signal })
+      .then(todos => {
+        setStatus('resolved');
+        setTodos(todos);
+      })
+      .catch(error => {
+        setStatus('rejected');
+        setError(error.message);
+      });
+
+    return () => {
+      abortController.abort();
+    };
+  }, []);
+
+  if (status === 'started') {
+    return <CardContainer>Loading...</CardContainer>;
+  }
+
+  if (status === 'rejected') {
+    return <CardContainer>{error}</CardContainer>;
+  }
+
+  return (
+    <CardContainer>
+      {todos.map(todo => {
+        return <Card key={todo.title}>{todo.title}</Card>;
+      })}
+    </CardContainer>
+  );
+}
+
+const CardContainer = styled.section`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  grid-gap: 1rem;
+  padding: 15px 20px;
+`;
+
+const Card = styled.article`
+  border: 1px solid turquoise;
+  height: 200px;
+`;
