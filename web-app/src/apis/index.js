898e0acc0da2ea6a2b8b67162cbc9ad8cca6607c
jorcalle11 <jorcalle11@gmail.com>
2020-07-25 14:43:06 -0500

    Accept fetch options

--- web-app/src/apis/index.js
+++ web-app/src/apis/index.js
@@ -1,6 +1,6 @@
-export async function fetchDataByEntity(entity = 'todos') {
+export async function fetchDataByEntity(entity = 'todos', options = {}) {
   const url = `https://jsonplaceholder.typicode.com/${entity}`;
-  const response = await fetch(url);
+  const response = await fetch(url, options);
 
   if (!response.ok) {
     return Promise.reject(response.body);
