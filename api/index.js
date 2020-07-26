f2b3dbf4a652fa4cdc4fa1845a424dcc660ccd7c
jorcalle11 <jorcalle11@gmail.com>
2020-07-25 20:34:51 -0500

    Update default server port

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const server = require('./server');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 server.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}`);
