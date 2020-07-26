75e9d2480e7ee129a3336e3c279e1e05196a7b01
jorcalle11 <jorcalle11@gmail.com>
2020-07-26 10:58:26 -0500

    Refactor CardList component

--- web-app/src/components/CardList/CardList.js
+++ web-app/src/components/CardList/CardList.js
@@ -2,72 +2,36 @@ import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-import Api from '../../apis';
+import Champions from '../Champions';
+import { ENTITIES } from '../../constants';
 
 export default function CardList({ selectedNavItem }) {
-  if (selectedNavItem === 'users') {
-    return 'users';
+  if (selectedNavItem === ENTITIES.CHAMPIONSHIPS) {
+    return ENTITIES.CHAMPIONSHIPS;
   }
 
-  if (selectedNavItem === 'posts') {
-    return 'posts';
+  if (selectedNavItem === ENTITIES.SHOWS) {
+    return ENTITIES.SHOWS;
   }
 
-  return <Todos />;
+  if (selectedNavItem === ENTITIES.SUPERSTARTS) {
+    return ENTITIES.SUPERSTARTS;
+  }
+
+  return (
+    <CardContainer>
+      <Champions />
+    </CardContainer>
+  );
 }
 
 CardList.propTypes = {
   selectedNavItem: PropTypes.string.isRequired
 };
 
-function Todos() {
-  const [todos, setTodos] = React.useState([]);
-  const [status, setStatus] = React.useState('started');
-  const [error, setError] = React.useState(null);
-
-  React.useEffect(() => {
-    const abortController = new AbortController();
-
-    Api.fetchDataByEntity('todos', { signal: abortController.signal })
-      .then(todos => {
-        setStatus('resolved');
-        setTodos(todos);
-      })
-      .catch(error => {
-        setStatus('rejected');
-        setError(error.message);
-      });
-
-    return () => {
-      abortController.abort();
-    };
-  }, []);
-
-  if (status === 'started') {
-    return <CardContainer>Loading...</CardContainer>;
-  }
-
-  if (status === 'rejected') {
-    return <CardContainer>{error}</CardContainer>;
-  }
-
-  return (
-    <CardContainer>
-      {todos.map(todo => {
-        return <Card key={todo.title}>{todo.title}</Card>;
-      })}
-    </CardContainer>
-  );
-}
-
 const CardContainer = styled.section`
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
   grid-gap: 1rem;
   padding: 15px 20px;
-`;
-
-const Card = styled.article`
-  border: 1px solid turquoise;
-  height: 200px;
 `;
